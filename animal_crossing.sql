06de10ab4d3043491f671de4b9363566bf7a7403
Shri2985 <61440659+Shri2985@users.noreply.github.com>
2020-05-23 13:30:47 -0400

    Add files via upload
    Uploading the Final SQL Script

--- animal_crossing.sql
+++ animal_crossing.sql
@@ -1,82 +1,68 @@
---creating table critic_review
-CREATE TABLE critic_review
-(
-    grade int not null,
-    publication VARCHAR(100) primary key ,
-    date VARCHAR(20)
-)
-
-drop table characters
-CREATE TABLE characters
-(
-    "Character" varchar(100) primary key,
-    "Species" VARCHAR(20),
-    "Description" VARCHAR(200)
-  
-)
+drop table critic_review;
+drop table villagers;
+drop table special_character;
+drop table characters;
+drop table sales_summary;
+drop table user_reviews;
+drop table items;
 
 
----create table special_characters
-CREATE TABLE special_characters
-(
-    "Name" varchar(100) Primary Key,
-    "Species" VARCHAR(20),
-    "Birthday" VARCHAR(200),
-    service VARCHAR(100)
-  
-)
+CREATE TABLE "critic_review" (
+  "publication" varchar PRIMARY KEY,
+  "grade" int,
+  "date" varchar
+);
 
--- DROP TABLE sales_summary;
-CREATE TABLE sales_summary
-(
-    "Name" VARCHAR,
-    "Year." integer,
-    "Units_Sold" decimal,
-    "Critic_Score" integer"
-)
+CREATE TABLE "characters" (
+  "version" VARCHAR,
+  "characters" VARCHAR PRIMARY KEY,
+  "species" VARCHAR(20),
+  "description" text
+);
 
--- DROP TABLE villagers;
-CREATE TABLE villagers
-(
-    name VARCHAR(50) Primary Key,
-    gender VARCHAR(6),
-    species VARCHAR(10),
-    birthday VARCHAR(20),
-    personality VARCHAR(10),
-    song VARCHAR(200),
-    phrase VARCHAR(20)
-)
+CREATE TABLE "special_character" (
+  "characters" varchar PRIMARY KEY,
+  "Species" VARCHAR,
+  "Birthday" VARCHAR,
+  "service" VARCHAR
+);
 
--- DROP TABLE user_reviews;
-CREATE TABLE user_reviews
-(
-    grade int,
-    "text" VARCHAR(100),
-    date VARCHAR(20)
-    )
+CREATE TABLE "sales_summary" (
+  "version" VARCHAR PRIMARY KEY,
+  "year" integer,
+  "units_sold" decimal,
+  "critic_score" integer
+);
+
+CREATE TABLE "villagers" (
+  "characters" VARCHAR(50) PRIMARY KEY,
+  "gender" VARCHAR(6),
+  "species" VARCHAR(10),
+  "birthday" VARCHAR(20),
+  "personality" VARCHAR(10),
+  "song" VARCHAR(200),
+  "phrase" VARCHAR(20)
+);
+
+CREATE TABLE "user_reviews" (
+  "serial_id" serial,
+  "grade" int,
+  "text" text,
+  "date" VARCHAR(20)
+);
+
+CREATE TABLE "items" (
+  "num_id" integer PRIMARY KEY,
+  "name" text,
+  "category" VARCHAR(20),
+  "sell_value" decimal,
+  "sell_currency" VARCHAR(10),
+  "buy_value" decimal,
+  "buy_currency" VARCHAR(10)
+);
+
+ALTER TABLE "special_character" ADD FOREIGN KEY ("characters") REFERENCES "characters" ("characters");
+
+ALTER TABLE "villagers" ADD FOREIGN KEY ("characters") REFERENCES "characters" ("characters");
 
 
-
-# items table
--- DROP TABLE items;
-CREATE TABLE items
-(
-    num_id integer,
-    "name" VARCHAR(20),
-    category VARCHAR(20),
-    sell_value decimal,
-    sell_currency VARCHAR(10),
-    buy_value decimal,
-    buy_currency VARCHAR(10),
-)
-
-__________________________________________________________________________________________________________________________________________
---comparison of critic review b/w Kaggle and Wiki
-
-select * from critic_review;
-
-select (sum(distinct grade )/count(distinct grade)) as critic_score_from_kaggle 
-from critic_review1 
-union all
-select cast((substring("Critic_Score" ,1,2) ) as integer) as critic_score_from_Wiki
-from sales_summary where "Name"='Animal Crossing: New Horizons' 
